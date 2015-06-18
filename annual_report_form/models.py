18b07cf2f29408f9cd8e2aa9bb43362d37c4a4f4
Chris Hartley <chris.hartley@anymouse.org>
2015-06-18 11:45:47 -0400

    pictures not required

--- annual_report_form/models.py
+++ annual_report_form/models.py
@@ -18,19 +18,17 @@ class annual_report(models.Model):
 	percent_completed = models.PositiveIntegerField(help_text="Roughly speaking, what percentage complete is this project?")
 
 	past_expenses = models.PositiveIntegerField(help_text="It is ok to use rough estimates.", verbose_name='Funds spent to date')
-	work_completed = models.TextField(max_length=5120, help_text="What work has been completed?", verbose_name='Written narative of the improvements made to date') 
+	work_completed = models.TextField(max_length=5120, help_text="What work has been completed?", verbose_name='Written narative of the improvements made to date')
 
-	work_remaining = models.TextField(max_length=5120, help_text="What work remains to be completed?", verbose_name='Work remaining') 
+	work_remaining = models.TextField(max_length=5120, help_text="What work remains to be completed?", verbose_name='Work remaining')
 	future_expenses = models.PositiveIntegerField(help_text="It is ok to use rough estimates.", verbose_name='Anticipated remaining expenses')
 
 	feedback = models.TextField(max_length=5120, help_text="Do you have any other comments on your project or feedback for Renew Indianapolis about your experience with our program?", verbose_name='Feedback')
 	certificate_of_completion_ready = models.BooleanField(verbose_name='Are you ready for a certificate of completion inspection?', default=False)
-	property_occupied = models.BooleanField(verbose_name='Is the property occupied?', default=False) 
+	property_occupied = models.BooleanField(verbose_name='Is the property occupied?', default=False)
 
-	front_exterior_picture = models.ImageField(upload_to=content_file_name)
-	back_exterior_picture = models.ImageField(upload_to=content_file_name)
-	kitchen_picture = models.ImageField(upload_to=content_file_name)
-	bathroom_picture = models.ImageField(upload_to=content_file_name)
+	front_exterior_picture = models.ImageField(upload_to=content_file_name, blank=True)
+	back_exterior_picture = models.ImageField(upload_to=content_file_name, blank=True)
+	kitchen_picture = models.ImageField(upload_to=content_file_name, blank=True)
+	bathroom_picture = models.ImageField(upload_to=content_file_name, blank=True)
 	other_picture = models.ImageField(upload_to=content_file_name, blank=True, help_text='Project photo of your choice (optional)')
-
-
