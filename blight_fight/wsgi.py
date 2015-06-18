a379264a5cce9811ae8610a25cae4f38431603cd
Chris Hartley <chris.hartley@anymouse.org>
2015-06-18 10:44:49 -0400

    Support devel/production environments with try import.

--- blight_fight/wsgi.py
+++ blight_fight/wsgi.py
@@ -11,21 +11,25 @@ import os
 import sys
 import site
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blight_fight.settings")
-#os.environ["DJANGO_SETTINGS_MODULE"] = "{{ project_name }}.settings"
-
-# Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('/home/django/.virtualenvs/blight_fight/local/lib/python2.7/site-packages')
-
-# Add the app's directory to the PYTHONPATH
-sys.path.append('/home/django/blight_fight')
-sys.path.append('/home/django/blight_fight/blight_fight')
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'blight_fight.settings'
 
-# Activate your virtual env
-activate_env=os.path.expanduser("/home/django/.virtualenvs/blight_fight/bin/activate_this.py")
-execfile(activate_env, dict(__file__=activate_env))
+try:
+	from settings_production import *
+
+	# Add the site-packages of the chosen virtualenv to work with
+	site.addsitedir('/home/django/.virtualenvs/blight_fight/local/lib/python2.7/site-packages')
+
+	# Add the app's directory to the PYTHONPATH
+	sys.path.append('/home/django/blight_fight')
+	sys.path.append('/home/django/blight_fight/blight_fight')
+
+
+	# Activate your virtual env
+	activate_env=os.path.expanduser("/home/django/.virtualenvs/blight_fight/bin/activate_this.py")
+	execfile(activate_env, dict(__file__=activate_env))
+
+except ImportError:
+	pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
