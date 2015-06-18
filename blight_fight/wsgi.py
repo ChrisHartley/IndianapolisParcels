1b7eb3f394dd94aeb07c3ae1e59c07f1367cce15
Chris Hartley <chris.hartley@anymouse.org>
2015-06-18 13:11:42 -0400

    re-arrange order to see if path can work

--- blight_fight/wsgi.py
+++ blight_fight/wsgi.py
@@ -17,14 +17,14 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'blight_fight.settings'
 # If we can import it then we are on production server and need to set up
 # the virtualenv. Sucks that these paths are hard-coded.
 try:
-	from settings_production import *
-
 	# Add the site-packages of the chosen virtualenv to work with
 	site.addsitedir('/home/django/.virtualenvs/blight_fight/local/lib/python2.7/site-packages')
 
 	# Add the app's directory to the PYTHONPATH
 	sys.path.append('/home/django/blight_fight')
 	sys.path.append('/home/django/blight_fight/blight_fight')
+
+	from settings_production import *
 
 
 	# Activate your virtual env
