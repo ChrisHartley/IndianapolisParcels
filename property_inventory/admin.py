d4904c91025ff62525c14c6b028069e6d62d7f28
Chris Hartley <chris.hartley@anymouse.org>
2015-07-20 09:13:44 -0400

    make admin searchable

--- property_inventory/admin.py
+++ property_inventory/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Property
 
-admin.site.register(Property)
+
+class PropertyAdmin(admin.ModelAdmin):
+    search_fields = ('parcel', 'streetAddress')
+
+admin.site.register(Property, PropertyAdmin)
