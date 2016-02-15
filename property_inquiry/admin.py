0c948acb4eca0df7d678c7939b6cbfa1e647bc57
Chris Hartley <chris.hartley@anymouse.org>
2016-02-15 09:20:54 -0800

    further tweak to admin

--- property_inquiry/admin.py
+++ property_inquiry/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import propertyInquiry
 
-admin.site.register(propertyInquiry)
-
+class propertyInquiryAdmin(admin.ModelAdmin):
+    fields = ('Property', 'user.email', 'timestamp')
+    
+admin.site.register(propertyInquiry, propertyInquiryAdmin)
