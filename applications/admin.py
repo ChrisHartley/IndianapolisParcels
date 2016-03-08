34af5b67d996b614125accee653dcd3570384662
Chris Hartley <chris.hartley@anymouse.org>
2016-03-08 12:43:21 -0500

    cnage dispaly options

--- applications/admin.py
+++ applications/admin.py
@@ -8,8 +8,8 @@ class UploadedFileInline(admin.TabularInline):
 
 
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('modified','Property', 'get_user', 'organization','status')
-    list_filter = ('status',)
+    list_display = ('modified','Property', 'get_user', 'organization','application_type','status')
+    list_filter = ('status','application_type')
     search_fields = ('Property__parcel', 'Property__streetAddress', 'user__email', 'user__first_name', 'user__last_name')
     inlines = [ UploadedFileInline]
     def get_user(self, obj):
