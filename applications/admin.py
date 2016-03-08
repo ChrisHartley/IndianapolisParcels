8d68c6dcb92f64525bf39028f7452ec9ecd579bf
Chris Hartley <chris.hartley@anymouse.org>
2016-03-08 10:03:54 -0500

    add uploadedfile inline

--- applications/admin.py
+++ applications/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Application
+from user_files.models import UploadedFile
+
+class UploadedFileInline(admin.TabularInline):
+    model = UploadedFile
+    extra = 0
 
 
 class ApplicationAdmin(admin.ModelAdmin):
     #    search_fields = ('parcel', 'streetAddress')
-    pass
+    inlines = [ UploadedFileInline]
 
 admin.site.register(Application, ApplicationAdmin)
